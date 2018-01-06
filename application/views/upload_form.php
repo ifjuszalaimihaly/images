9ecc685c049ab5054b3bd434796d19ba462d80df
Unknown <ifjuszalaimihaly@gmail.com>
2018-01-06 22:40:16 +0100

    can upload with jquery

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -4,13 +4,13 @@
 </head>
 <body>
 
-<form action="http://localhost/images/index.php/upload/do_upload" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+<form enctype="multipart/form-data" method="post" accept-charset="utf-8">
 
-<input type="file" name="userfile" size="20" />
+    <input type="file" name="userfile" id="userfile" size="20"/>
 
-<br /><br />
+    <br/><br/>
 
-<input type="submit" value="upload" />
+    <input type="button" value="upload" id="upload"/>
 
 </form>
 
@@ -18,6 +18,29 @@
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous"></script>
+<script>
+    $('#upload').on('click',
+        function () {
+            var fileData = $('#userfile').prop('files')[0];
+            var formData = new FormData();
+            formData.append('userfile', fileData);
+            $.ajax({
+                url: 'http://localhost/images/index.php/upload/do_upload',
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                type: 'post',
+                success: function () {
+                    alert('success');
+                },
+                error: function () {
+                    alert('error');
+                }
+            });
+        }
+    );
+</script>
 
 </body>
 </html>
