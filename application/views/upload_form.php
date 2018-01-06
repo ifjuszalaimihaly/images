f4deb1e9dee7c4ee13b88695cee92617d1184b3c
Unknown <ifjuszalaimihaly@gmail.com>
2018-01-06 22:18:23 +0100

    added jquery

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -14,5 +14,10 @@
 
 </form>
 
+<script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
