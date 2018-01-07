905acb547623ad99790de28f7f30067b2a48c244
Unknown <ifjuszalaimihaly@gmail.com>
2018-01-07 13:47:53 +0100

    added bootstrap

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Upload Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <style>
         .example {
             padding: 10px;
@@ -29,6 +30,7 @@
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script>
 
     function upload(file) {
