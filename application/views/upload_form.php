338397fc67edfdd827b6bc5a0515058053cf5242
Unknown <ifjuszalaimihaly@gmail.com>
2018-01-08 18:22:05 +0100

    replaced file input

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -18,14 +18,18 @@
             font: 20pt bold;
             color: #bbb;
         }
+        #file-input{
+            margin: auto;
+        }
     </style>
 </head>
 <body>
 <div class="container">
     <div class="example row">
-        <div id="dropzone">Drop files here</div>
+        <div id="dropzone">Húzza ide a fájlokat
+            <input class="" type="file" id="file-input" text="Select files" name="files[]">
+        </div>
     </div>
-    <input class="" type="file" id="file-input" name="files[]" multiple="">
     <div style="display: none" class="progress row">
         <div class="progress-bar progress-bar-striped active" role="progressbar"
              aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%; display: none">
